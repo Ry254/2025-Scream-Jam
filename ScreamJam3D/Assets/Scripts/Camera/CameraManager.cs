e0d64e7224d6f4216082ad3dc8edb87eaf95edb8
ZRedCon <131729898+ZRedCon@users.noreply.github.com>
2025-10-08 22:32:54 -0400

    Added ScriptTemplates

---
+++ ScreamJam3D/Assets/Scripts/Camera/CameraManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    // Singleton Implementation
+    public static CameraManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
