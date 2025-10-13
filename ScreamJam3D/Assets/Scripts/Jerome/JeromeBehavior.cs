8f578a73fb9e439713a38b7cd1ef17793b04332d
ahf_git <ahalim.findikli@gmail.com>
2025-10-13 09:14:22 -0400

    Jerome behavior added, UI scales better, main test now has all features
    side note, the deathManager CauseDeath method seems to break the game half the time

--- ScreamJam3D/Assets/Scripts/Jerome/JeromeBehavior.cs
+++ ScreamJam3D/Assets/Scripts/Jerome/JeromeBehavior.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class JeromeBehavior : MonoBehaviour
 {
@@ -7,15 +9,21 @@ public class JeromeBehavior : MonoBehaviour
 
     //time in milliseconds
     [SerializeField]
-    private int sleepTime = 1000, awakeTime = 1000;
-    
+    private int sleepTime = 10000, awakeTime = 10000;
+
+    private PlayerLookState currentLook;
+
+    private bool wait;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         sleepTimer = Stopwatch.StartNew();
-        awakeTimer = Stopwatch.StartNew();
-        
-        sleepTimer.Start();
+        awakeTimer = new Stopwatch();
+
+        currentLook = CameraManager.Instance.CurrentLookState;
+        CameraManager.Instance.OnCameraChange += Look;
+        wait = false;
     }
 
     // Update is called once per frame
@@ -24,15 +32,35 @@ public class JeromeBehavior : MonoBehaviour
         if (sleepTimer.ElapsedMilliseconds >= sleepTime && sleepTimer.IsRunning)
         {
             sleepTimer.Stop();
-            awakeTimer.Start();
-            //enable kill switch
+            if (currentLook == PlayerLookState.RightWindow || currentLook == PlayerLookState.Fridge)
+            {
+                wait = true;
+            }
+            else
+            {
+                awakeTimer.Restart();
+            }
         }
 
         if (awakeTimer.ElapsedMilliseconds >= awakeTime && awakeTimer.IsRunning)
         {
             awakeTimer.Stop();
-            sleepTimer.Start();
-            //disable kill switch
+            sleepTimer.Restart();
+            wait = false;
         }
+    }
+
+    public void Look(PlayerLookState look)
+    {
+        if (wait && look != PlayerLookState.RightWindow && look != PlayerLookState.Fridge)
+        {
+            awakeTimer.Restart();
+            wait = false;
+        }
+        else if (look == PlayerLookState.RightWindow && awakeTimer.IsRunning)
+        {
+            DeathManager.Instance.CauseDeath("Jerome was awake.", "maybe sing some lullabys next time.");
+        }
+        currentLook = look;
     }
 }
